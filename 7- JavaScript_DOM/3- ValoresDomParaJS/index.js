7b2c6b88db2552ffd5b2a57f7c36f4cb411dd9c0
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-11-07 20:42:44 -0300

    Enviando valores do DOM para o JS - JS DOM

---
+++ 7- JavaScript_DOM/3- ValoresDomParaJS/index.js
@@ -0,0 +1,13 @@
+// function register(element) {
+//   const nome = element.children.usernameName.value; // Pelo atributo name
+//   // const nome = element.children.username.value;  // Pelo atributo id
+//   const senha = element.children.password.value;
+//   const confirmacao = element.children.passwordConfirmation.value;
+
+//   if (senha == confirmacao) alert("Usuário " + nome + " registrado!");
+//   else alert("As senhas não conferem!");
+// }
+
+function register(element) {
+  console.log(element);
+}
