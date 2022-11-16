03dfa114c75d204f7080e71e9e80d79cc7a1965b
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-11-16 10:11:02 -0300

    Finalização da calculadora JS - JS DOM

--- 7- JavaScript_DOM/10- CalculadoraV2/index.js
+++ 7- JavaScript_DOM/10- CalculadoraV2/index.js
@@ -53,6 +53,47 @@ document.getElementById("btnC").addEventListener("click", function () {
 
 // Calculando
 function calculate() {
+  result.classList.add("error");
+  result.value = "Error";
   const resulted = eval(input.value);
+
+  console.log("Chegou aqui!");
+  result.classList.remove("error");
   result.value = resulted;
 }
+
+// Alterando tema
+const root = document.querySelector(":root");
+const tema = document.getElementById("theme-btn");
+
+tema.addEventListener("click", function () {
+  if (tema.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    tema.dataset.theme = "light";
+    tema.innerText = "Tema escuro";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    tema.dataset.theme = "dark";
+    tema.innerText = "Tema claro";
+  }
+});
+
+// Copiando resultado
+const btnCopy = document.getElementById("copy-btn");
+
+btnCopy.addEventListener("click", function () {
+  if (btnCopy.innerHTML === "Copy") {
+    btnCopy.innerText = "Copied";
+    btnCopy.classList.add("success");
+    navigator.clipboard.writeText(result.value);
+  } else {
+    btnCopy.innerText = "Copy";
+    btnCopy.classList.remove("success");
+  }
+});
