cda9cc8b07b6e229a04a297bc90b8f8e1671e934
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-11-20 21:02:26 -0300

    Média ponderada execício estatística - JS Moderno

--- 8- JavaScript_Moderno/8- ExercicioEstatistica/index.js
+++ 8- JavaScript_Moderno/8- ExercicioEstatistica/index.js
@@ -3,7 +3,7 @@ let seqNumbers = [];
 
 for (let i = 0; i < 10; i++) seqNumbers[i] = Math.floor(Math.random() * 10);
 
-console.log(seqNumbers);
+console.log(`Sequência de números: ${seqNumbers}`);
 
 // Calculando média aritmética
 function mediaAritmetica(...numbers) {
@@ -15,4 +15,29 @@ function mediaAritmetica(...numbers) {
   return soma / numbers.length;
 }
 
-console.log(mediaAritmetica(...seqNumbers)); // O "..." passa um elemento por vez à função
+console.log(mediaAritmetica(...seqNumbers));
+// O "..." passa um elemento por vez à função
+
+// Calculando a média ponderada
+const pesos = [];
+
+for (let i = 0; i < 10; i++) pesos[i] = i + 1;
+
+console.log(`Pesos: ${pesos}`);
+
+function mediaPonderada(...numbers) {
+  let dividendo = numbers.reduce(
+    (acumulador, valorAtual, indiceAtual) =>
+      acumulador + valorAtual * pesos[indiceAtual],
+    0
+  );
+
+  let divisor = pesos.reduce(
+    (acumulador, valorAtual) => acumulador + valorAtual,
+    0
+  );
+
+  return (dividendo / divisor).toFixed(2);
+}
+
+console.log(mediaPonderada(...seqNumbers));
