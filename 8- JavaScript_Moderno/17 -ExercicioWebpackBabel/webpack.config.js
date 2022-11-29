1081fcce0a89770469d28bf7bcec40844b0075d5
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-11-29 19:14:07 -0300

    Webpack-dev-server - JS Moderno

--- 8- JavaScript_Moderno/17 -ExercicioWebpackBabel/webpack.config.js
+++ 8- JavaScript_Moderno/17 -ExercicioWebpackBabel/webpack.config.js
@@ -1,4 +1,14 @@
+const path = require("path");
+
 module.exports = {
+  // O dev server faz parte da aula sobre webpack-dev-server
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8000,
+  },
   entry: {
     index: "./src/index.js",
     hello: "./src/hello.js",
