f94b3ef4b3b72f4e93a39fa2b23680be2965c427
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-11-29 12:42:18 -0300

    Plugins webpack - JS Moderno

--- 8- JavaScript_Moderno/16- Webpack/webpack.config.js
+++ 8- JavaScript_Moderno/16- Webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: {
     index: "./src/index.js",
@@ -7,8 +9,9 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
