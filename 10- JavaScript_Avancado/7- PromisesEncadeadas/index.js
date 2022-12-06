78842db7401abc932d43cd6dffea02f48bdeb653
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-12-06 14:11:06 -0300

    Promises encadeadas - JS Avançado

---
+++ 10- JavaScript_Avancado/7- PromisesEncadeadas/index.js
@@ -0,0 +1,44 @@
+const spaceship = {
+  name: "Rontaro",
+  currentBateryCharge: 50,
+  consumptionPerKms: 0.00008,
+};
+const updateBateryCharge = function (chargeConsumed) {
+  return new Promise((resolve, reject) => {
+    spaceship.currentBateryCharge -= chargeConsumed;
+    if (spaceship.currentBateryCharge > 0) {
+      resolve(spaceship.currentBateryCharge);
+    } else {
+      reject("Bateria esgotada! Nave será desativada em alguns segundos.");
+    }
+  });
+};
+const calculateBateryConsumption = function (velocity, seconds) {
+  return new Promise((resolve, reject) => {
+    if (spaceship.consumptionPerKms <= 0 || velocity <= 0) {
+      reject("Nave está parada!");
+    } else {
+      let chargeConsumed = spaceship.consumptionPerKms * velocity * seconds;
+      resolve(chargeConsumed);
+    }
+  });
+};
+
+// calculateBateryConsumption(90, 300)
+//   .then((charge) => {
+//     updateBateryCharge(charge)
+//       .then((currentCharge) => {
+//         console.log(currentCharge);
+//       })
+//       .catch((error) => console.log(error));
+//   })
+//   .catch((error) => console.log(error));
+
+calculateBateryConsumption(90, 300)
+  .then((charge) => {
+    return updateBateryCharge(charge);
+  })
+  .then((currentCharge) => {
+    console.log(currentCharge);
+  })
+  .catch((error) => console.log(error));
