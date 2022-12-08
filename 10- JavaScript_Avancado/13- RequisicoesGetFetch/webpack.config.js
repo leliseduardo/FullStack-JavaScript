884eb1eda03e11584e232837d52def3efe98d6dc
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-12-08 09:00:11 -0300

    Requisições get fetch, preparação ambiente - JS Avançado

---
+++ 10- JavaScript_Avancado/13- RequisicoesGetFetch/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+
+  watch: true,
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+  },
+};
