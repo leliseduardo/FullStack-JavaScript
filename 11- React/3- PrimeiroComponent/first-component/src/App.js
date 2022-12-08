dcce8cb6e4a6ee26817478c824a0f29cb55982bb
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-12-08 19:34:20 -0300

    Criando o primeiro componente - React

---
+++ 11- React/3- PrimeiroComponent/first-component/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWorld from "./components/Hello-World";
+
+// function HelloWorld() {
+//   return <h1>HelloWorld</h1>;
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
