75e379e5c79f442db7af79e4d8dcb99e630859d8
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-12-12 17:54:12 -0300

    Criando uma classe com estado - React

--- 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/index.js
+++ 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/index.js
@@ -9,32 +9,109 @@ import Planet from "./planet";
 //   console.log(`Clicou no planeta ${name}`);
 // };
 
-const Planets = () => {
-  return (
-    <Fragment>
-      <h3>Planet List</h3>
-      {/* <button onClick={showMessage}>Message</button> */}
-      <hr />
-      <Planet
-        name="Mercúrio"
-        description="Mercúrio é um planeta do sistema solar, que pertence à Via Láctea."
-        link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-        // clickOnPlanet={clickOnPlanet}
-        title_with_underline={false}
-        grayScale={true}
-      />
-      <Planet
-        name="Plutão"
-        // description="Plutão é um planeta anão do sistema solar, que pertence à Via Láctea."
-        link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-        // clickOnPlanet={clickOnPlanet}
-        title_with_underline={true}
-        grayScale={false}
-      />
-    </Fragment>
-  );
-};
+// Renderização condicional
+// const Planets = () => {
+//   return (
+//     <Fragment>
+//       <h3>Planet List</h3>
+//       {/* <button onClick={showMessage}>Message</button> */}
+//       <hr />
+//       <Planet
+//         name="Mercúrio"
+//         description="Mercúrio é um planeta do sistema solar, que pertence à Via Láctea."
+//         link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+//         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
+//         // clickOnPlanet={clickOnPlanet}
+//         title_with_underline={false}
+//         grayScale={true}
+//       />
+//       <Planet
+//         name="Plutão"
+//         // description="Plutão é um planeta anão do sistema solar, que pertence à Via Láctea."
+//         link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
+//         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
+//         // clickOnPlanet={clickOnPlanet}
+//         title_with_underline={true}
+//         grayScale={false}
+//       />
+//     </Fragment>
+//   );
+// };
+
+// Classes com estado
+class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planets: [
+        {
+          name: "Mercúrio",
+          description:
+            "Mercúrio é um planeta do sistema solar, que pertence à Via Láctea.",
+          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
+        },
+        {
+          name: "Plutão",
+          description:
+            "Plutão é um planeta anão do sistema solar, que pertence à Via Láctea.",
+          link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
+        },
+      ],
+    };
+  }
+
+  removeLast = () => {
+    let new_planets = [...this.state.planets];
+    new_planets.pop();
+    this.setState((state) => ({
+      planets: new_planets,
+    }));
+  };
+
+  duplicateLastPlanet = () => {
+    let last_planet = this.state.planets[this.state.planets.length - 1];
+    this.setState((state) => ({
+      planets: [...this.state.planets, last_planet],
+    }));
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Planet List</h3>
+        <button onClick={this.removeLast}>Remove last</button>
+        <button onClick={this.duplicateLastPlanet}>Duplicate last</button>
+        <hr />
+        {/* <Planet
+          name={this.state.planets[0].name}
+          description={this.state.planets[0].description}
+          link={this.state.planets[0].link}
+          img_url={this.state.planets[0].img_url}
+        />
+        <Planet
+          name={this.state.planets[1].name}
+          description={this.state.planets[1].description}
+          link={this.state.planets[1].link}
+          img_url={this.state.planets[1].img_url}
+        /> */}
+        {/* Outra forma */}
+        {this.state.planets.map((planet, i) => (
+          <Planet
+            key={i}
+            name={planet.name}
+            description={planet.description}
+            link={planet.link}
+            img_url={planet.img_url}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
 
 export default Planets;
--- 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/planet/index.js
+++ 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/planet/index.js
@@ -14,12 +14,39 @@ import DescriptionWithLink from "../../shared/description-with-link";
 // };
 
 // Aula sobre renderização condicional
-const Planet = (props) => {
-  let names = ["a", "b", "c", "d"];
-  // let satellites = names.map((n, i) => <li key={i}>Satélite {n}</li>);
-  // satellites será um array formado pelo processamento de cada elemento.
-  // Nesse caso, será formado por elementos "li" com o nome de cada elemento
+// const Planet = (props) => {
+//   let names = ["a", "b", "c", "d"];
+//   // let satellites = names.map((n, i) => <li key={i}>Satélite {n}</li>);
+//   // satellites será um array formado pelo processamento de cada elemento.
+//   // Nesse caso, será formado por elementos "li" com o nome de cada elemento
 
+//   let title;
+//   if (props.title_with_underline)
+//     title = (
+//       <h1>
+//         <u>{props.name}</u>
+//       </h1>
+//     );
+//   else title = <h1>{props.name}</h1>;
+
+//   return (
+//     <div>
+//       {title}
+//       <DescriptionWithLink description={props.description} link={props.link} />
+//       <h3>Satélites</h3>
+//       {/* <ul>{satellites}</ul> */}
+//       <ul>
+//         {names.map((n, i) => (
+//           <li key={i}>Satélite {n}</li>
+//         ))}
+//       </ul>
+//       <GrayIMG img_url={props.img_url} grayScale={props.grayScale} />
+//     </div>
+//   );
+// };
+
+// Criando classe com estado
+const Planet = (props) => {
   let title;
   if (props.title_with_underline)
     title = (
@@ -33,13 +60,6 @@ const Planet = (props) => {
     <div>
       {title}
       <DescriptionWithLink description={props.description} link={props.link} />
-      <h3>Satélites</h3>
-      {/* <ul>{satellites}</ul> */}
-      <ul>
-        {names.map((n, i) => (
-          <li key={i}>Satélite {n}</li>
-        ))}
-      </ul>
       <GrayIMG img_url={props.img_url} grayScale={props.grayScale} />
     </div>
   );
