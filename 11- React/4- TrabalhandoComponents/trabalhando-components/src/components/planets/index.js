6d563c8aae8b789b3821fcf3c4a1534f6315e8a9
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-12-16 16:56:51 -0300

    Refatorando state com Hooks - React

--- 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/index.js
+++ 11- React/4- TrabalhandoComponents/trabalhando-components/src/components/planets/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Planet from "./planet";
 
 // const showMessage = () => {
@@ -38,103 +38,149 @@ import Planet from "./planet";
 //   );
 // };
 
+// // Aula lifecycle
+// async function getPlanets() {
+//   let response = await fetch("http://localhost:3000/api/planets.json");
+//   let data = await response.json();
+//   return data;
+// }
+
+// // Classes com estado
+// class Planets extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+// Aula estados
+//   this.state = {
+//     planets: [
+//       {
+//         name: "Mercúrio",
+//         description:
+//           "Mercúrio é um planeta do sistema solar, que pertence à Via Láctea.",
+//         link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+//         img_url:
+//           "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
+//       },
+//       {
+//         name: "Plutão",
+//         description:
+//           "Plutão é um planeta anão do sistema solar, que pertence à Via Láctea.",
+//         link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
+//         img_url:
+//           "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
+//       },
+//     ],
+//   };
+// }
+
 // Aula lifecycle
-async function getPlanets() {
-  let response = await fetch("http://localhost:3000/api/planets.json");
-  let data = await response.json();
-  return data;
-}
+//     this.state = {
+//       planets: [],
+//     };
+//   }
 
-// Classes com estado
-class Planets extends React.Component {
-  constructor(props) {
-    super(props);
+//   componentDidMount() {
+//     getPlanets().then((data) => {
+//       this.setState((state) => ({
+//         planets: data["planets"],
+//       }));
+//     });
+//   }
 
-    // Aula estados
-    //   this.state = {
-    //     planets: [
-    //       {
-    //         name: "Mercúrio",
-    //         description:
-    //           "Mercúrio é um planeta do sistema solar, que pertence à Via Láctea.",
-    //         link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
-    //         img_url:
-    //           "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
-    //       },
-    //       {
-    //         name: "Plutão",
-    //         description:
-    //           "Plutão é um planeta anão do sistema solar, que pertence à Via Láctea.",
-    //         link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
-    //         img_url:
-    //           "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
-    //       },
-    //     ],
-    //   };
-    // }
+//   removeLast = () => {
+//     let new_planets = [...this.state.planets];
+//     new_planets.pop();
+//     this.setState((state) => ({
+//       planets: new_planets,
+//     }));
+//   };
 
-    // Aula lifecycle
-    this.state = {
-      planets: [],
-    };
-  }
+//   duplicateLastPlanet = () => {
+//     let last_planet = this.state.planets[this.state.planets.length - 1];
+//     this.setState((state) => ({
+//       planets: [...this.state.planets, last_planet],
+//     }));
+//   };
 
-  componentDidMount() {
-    getPlanets().then((data) => {
-      this.setState((state) => ({
-        planets: data["planets"],
-      }));
-    });
-  }
+//   render() {
+//     return (
+//       <Fragment>
+//         <h1>Planet List</h1>
+//         <button onClick={this.removeLast}>Remove last</button>
+//         <button onClick={this.duplicateLastPlanet}>Duplicate last</button>
+//         <hr />
+//         {/* <Planet
+//           name={this.state.planets[0].name}
+//           description={this.state.planets[0].description}
+//           link={this.state.planets[0].link}
+//           img_url={this.state.planets[0].img_url}
+//         />
+//         <Planet
+//           name={this.state.planets[1].name}
+//           description={this.state.planets[1].description}
+//           link={this.state.planets[1].link}
+//           img_url={this.state.planets[1].img_url}
+//         /> */}
+//         {/* Outra forma */}
+//         {this.state.planets.map((planet, i) => (
+//           <Planet
+//             key={i}
+//             id={planet.id}
+//             name={planet.name}
+//             description={planet.description}
+//             link={planet.link}
+//             img_url={planet.img_url}
+//           />
+//         ))}
+//       </Fragment>
+//     );
+//   }
+// }
 
-  removeLast = () => {
-    let new_planets = [...this.state.planets];
+// export default Planets;
+
+const Planets = () => {
+  const [planets, setPlanets] = useState([
+    {
+      id: "mars",
+      name: "Mars",
+      description:
+        "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.",
+      img_url:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
+      link: "https://en.wikipedia.org/wiki/Mars",
+    },
+  ]);
+
+  const removeLast = () => {
+    let new_planets = [...planets];
     new_planets.pop();
-    this.setState((state) => ({
-      planets: new_planets,
-    }));
+    setPlanets(new_planets);
   };
 
-  duplicateLastPlanet = () => {
-    let last_planet = this.state.planets[this.state.planets.length - 1];
-    this.setState((state) => ({
-      planets: [...this.state.planets, last_planet],
-    }));
+  const duplicateLastPlanet = () => {
+    let last_planet = planets[planets.length - 1];
+    setPlanets([...planets, last_planet]);
   };
 
-  render() {
-    return (
-      <Fragment>
-        <h1>Planet List</h1>
-        <button onClick={this.removeLast}>Remove last</button>
-        <button onClick={this.duplicateLastPlanet}>Duplicate last</button>
-        <hr />
-        {/* <Planet
-          name={this.state.planets[0].name}
-          description={this.state.planets[0].description}
-          link={this.state.planets[0].link}
-          img_url={this.state.planets[0].img_url}
-        />
+  return (
+    <Fragment>
+      <h1>Planet List</h1>
+      <button onClick={removeLast}>Remove last</button>
+      <button onClick={duplicateLastPlanet}>Duplicate last</button>
+      <hr />
+      {planets.map((planet, i) => (
         <Planet
-          name={this.state.planets[1].name}
-          description={this.state.planets[1].description}
-          link={this.state.planets[1].link}
-          img_url={this.state.planets[1].img_url}
-        /> */}
-        {/* Outra forma */}
-        {this.state.planets.map((planet, i) => (
-          <Planet
-            key={i}
-            id={planet.id}
-            name={planet.name}
-            description={planet.description}
-            link={planet.link}
-            img_url={planet.img_url}
-          />
-        ))}
-      </Fragment>
-    );
-  }
-}
+          key={i}
+          id={planet.id}
+          name={planet.name}
+          description={planet.description}
+          link={planet.link}
+          img_url={planet.img_url}
+        />
+      ))}
+    </Fragment>
+  );
+};
 
 export default Planets;
