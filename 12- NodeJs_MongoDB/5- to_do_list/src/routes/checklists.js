4fe0491f41341e8c4b196044eebdfc1943b0506b
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-02 10:58:14 -0300

    Parâmetros nas rotas com Express - NodeJS

--- 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
+++ 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+// Rota com requisição GET
 router.get("/", (req, res) => {
   console.log("Olá");
   res.send();
 });
 
+// Rota com requisição POST
 router.post("/", (req, res) => {
   console.log(req.body);
   res.status(200).send(req.body);
+});
+
+// Rotas com parâmetro
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(`Id: ${req.params.id}`);
 });
 
 module.exports = router;
