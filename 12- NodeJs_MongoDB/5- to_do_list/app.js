72b928c59e6e7fa5355f5c57323502d8fc085605
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-02 10:44:38 -0300

    Concentrando rotas com Express - NodeJS

--- 12- NodeJs_MongoDB/5- to_do_list/app.js
+++ 12- NodeJs_MongoDB/5- to_do_list/app.js
@@ -1,26 +1,28 @@
 const express = require("express");
+const checkListRouter = require("./src/routes/checklists");
 
 const app = express();
-
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-};
-
-app.use(log);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Minha lista de tarefas</h1>");
-});
-
-app.get("/json", (req, res) => {
-  console.log(req.body);
-  res.json({ title: "Tarefa X", done: true });
-});
+app.use("/checklists", checkListRouter);
 
 app.listen(3000, () => {
   console.log("O servidor foi iniciado");
 });
+
+// const log = (req, res, next) => {
+//   console.log(req.body);
+//   console.log(`Data: ${Date.now()}`);
+//   next();
+// };
+
+// app.use(log);
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Minha lista de tarefas</h1>");
+// });
+
+// app.get("/json", (req, res) => {
+//   console.log(req.body);
+//   res.json({ title: "Tarefa X", done: true });
+// });
