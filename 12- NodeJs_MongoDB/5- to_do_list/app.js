6a77034022b206678c41bebaaca75090ad0f831d
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-08 10:46:16 -0300

    Pacote MethodOverride - NodeJS

--- 12- NodeJs_MongoDB/5- to_do_list/app.js
+++ 12- NodeJs_MongoDB/5- to_do_list/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const checkListRouter = require("./src/routes/checklists");
 const rootRouter = require("./src/routes/index");
+const methodOverride = require("method-override");
 require("./config/database");
 const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
