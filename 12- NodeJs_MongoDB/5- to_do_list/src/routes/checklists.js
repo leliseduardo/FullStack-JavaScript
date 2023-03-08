7f28e6ec5ee7e5c1e35081240a9cc4f50751218e
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-08 11:54:33 -0300

    Aualização de dados com PUT - NodeJS

--- 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
+++ 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
@@ -29,6 +29,17 @@ router.get("/new", async (req, res) => {
   }
 });
 
+router.get("/:id/edit", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", {
+      error: "Erro ao exibir a edição de lista de tarefas",
+    });
+  }
+});
+
 // Rota com requisição POST
 router.post("/", async (req, res) => {
   let { name } = req.body.checklist;
@@ -58,17 +69,17 @@ router.get("/:id", async (req, res) => {
 
 // Rotas com PUT
 router.put("/:id", async (req, res) => {
-  let { name } = req.body;
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
 
   try {
-    let checklist = await Checklist.findByIdAndUpdate(
-      req.params.id,
-      { name },
-      { new: true }
-    );
-    res.status(200).json(checklist);
+    await checklist.update({ name });
+    res.redirect("/checklists");
   } catch (error) {
-    res.status(422).json(error);
+    let errors = error.errors;
+    res
+      .status(422)
+      .render("checklists/edit", { checklist: { ...checklist, errors } });
   }
 });
 
