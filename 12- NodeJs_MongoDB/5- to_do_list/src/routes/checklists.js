0f83c21ecab5754164deee8eba10141b4dddfad0
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-08 10:05:31 -0300

    Enviando dados de um formulário com POST - NodeJS

--- 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
+++ 12- NodeJs_MongoDB/5- to_do_list/src/routes/checklists.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const checklist = require("../models/checklist");
 
 const router = express.Router();
 
@@ -17,15 +18,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/new", async (req, res) => {
+  try {
+    let checklist = new Checklist();
+    res.status(200).render("checklists/new", { checklist: checklist });
+  } catch (error) {
+    res
+      .status(500)
+      .render("pages/error", { error: "Erro ao carregar o formulário" });
+  }
+});
+
 // Rota com requisição POST
 router.post("/", async (req, res) => {
-  let { name } = req.body;
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({ name });
 
   try {
-    let checklist = await Checklist.create({ name });
-    res.status(200).json(checklist);
+    await checklist.save();
+    res.redirect("/checklists");
   } catch (error) {
-    res.status(422).json(error);
+    res
+      .status(422)
+      .render("checklists/new", { checklist: { ...checklist, error } });
   }
 });
 
@@ -36,7 +51,7 @@ router.get("/:id", async (req, res) => {
     res.status(200).render("checklists/show", { checklist: checklist });
   } catch (error) {
     res
-      .status(200)
+      .status(500)
       .render("pages/error", { error: "Erro ao exibisr as listas de tarefas" });
   }
 });
