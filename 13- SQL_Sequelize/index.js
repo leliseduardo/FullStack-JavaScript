2c89154987c7a29f965c4674370ab8a11a3c5909
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-14 11:59:27 -0300

    Atualizando valores via Sequelize - SQL/Sequelize

--- 13- SQL_Sequelize/index.js
+++ 13- SQL_Sequelize/index.js
@@ -1,18 +1,27 @@
 (async () => {
   const Planet = require("./models/Planet");
 
+  // Adicionando
   // const newPlanet = await Planet.create({
   //   name: "Venus",
   //   position: 5,
   // });
 
+  // Pesquisando
   // const seePlanets = await Planet.findAll();
   // const seePlanets = await Planet.findByPk(1);
-  const seePlanets = await Planet.findAll({
-    where: {
-      name: "Terra",
-    },
-  });
+  // const seePlanets = await Planet.findAll({
+  //   where: {
+  //     name: "Terra",
+  //   },
+  // });
 
-  console.log(seePlanets);
+  // Atualizando
+  const updatePlanets = await Planet.findByPk(4);
+  updatePlanets.name = "Jupter";
+  await updatePlanets.save();
+
+  // console.log(newPlanet);
+  // console.log(seePlanets);
+  console.log(updatePlanets);
 })();
