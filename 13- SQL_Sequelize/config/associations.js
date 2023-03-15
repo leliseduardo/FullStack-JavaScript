ba34e30f5ddd1c5a767212a29d523be646078b43
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-15 11:30:50 -0300

    Exercício Has Many - SQL/Sequelize

--- 13- SQL_Sequelize/config/associations.js
+++ 13- SQL_Sequelize/config/associations.js
@@ -1,7 +1,10 @@
 const Planet = require("../models/Planet");
 const Satelite = require("../models/Satelite");
 
-Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
-Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planets" });
+// Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+// Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
+
+Planet.hasMany(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
 
 module.exports = { Planet, Satelite };
