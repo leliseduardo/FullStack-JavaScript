fc9297dce992c2836e4bcbcabb47696861a645bf
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-15 12:25:58 -0300

    Exercício Many to Many - SQL/Sequelize

--- 13- SQL_Sequelize/config/associations.js
+++ 13- SQL_Sequelize/config/associations.js
@@ -1,10 +1,24 @@
 const Planet = require("../models/Planet");
 const Satelite = require("../models/Satelite");
+const Cap = require("../models/Cap");
+const Spaceship = require("../models/Spaceship");
 
-// Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Planet.hasOne(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
+
+// Planet.hasMany(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 // Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
 
-Planet.hasMany(Satelite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
-Satelite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
+Cap.belongsToMany(Spaceship, {
+  foreingKey: "capId",
+  through: "capSpaceship",
+  as: "spaceships",
+});
+
+Spaceship.belongsToMany(Cap, {
+  foreingKey: "spaceshipId",
+  through: "capSpaceship",
+  as: "caps",
+});
 
 module.exports = { Planet, Satelite };
--- 13- SQL_Sequelize/src/routes.js
+++ 13- SQL_Sequelize/src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const PlanetController = require("../Controller/PlanetController");
 const SateliteController = require("../Controller/SateliteController");
+const CapController = require("../Controller/CapController");
+const SpaceshipController = require("../Controller/SpaceshipController");
 
 routes.post("/planets", PlanetController.store);
 routes.get("/planets", PlanetController.index);
@@ -11,5 +13,11 @@ routes.delete("/planets/:id", PlanetController.delete);
 
 routes.post("/planets/:planetId/satelites", SateliteController.store);
 routes.get("/planets/:planetId/satelites", SateliteController.index);
+
+routes.post("/cap", CapController.store);
+routes.get("/cap", CapController.index);
+
+routes.post("/caps/:capId/spaceships", SpaceshipController.store);
+routes.get("/caps/:capId/spaceships", SpaceshipController.index);
 
 module.exports = routes;
