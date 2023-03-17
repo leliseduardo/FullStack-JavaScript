151eedd105d47789121c889b29df78d797cafa64
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-17 13:58:50 -0300

    Exercício 1, tipos, parte 2 - TS

--- 14- TypeScript/3- Exercicio1_Tipos/src/index.ts
+++ 14- TypeScript/3- Exercicio1_Tipos/src/index.ts
@@ -18,7 +18,7 @@ function findSpaceship(name: string) {
   let spaceship: {
     name: string;
     pilot: string;
-    crewLimit: string;
+    crewLimit: number;
     crew: string[];
     inMission: boolean;
   };
@@ -54,5 +54,123 @@ function sendInMission(spaceship: {
   } else {
     spaceship.inMission = true;
     alert(`${spaceship.name} foi enviada para a missão.`);
+  }
+}
+
+function firstMenuOption() {
+  const name = prompt(`Qual o nome da nave a ser registrada?`);
+  const pilot = prompt(`Qual o nome do piloto da nave ${name}`);
+  const crewLimit = Number(
+    prompt(`Quantos tripulantes da nave ${name} suporta?`)
+  );
+
+  const confirmation = confirm(
+    `Confirma o registro da nave ${name}\n Piloto: ${pilot} \n Limite da tripulação: ${crewLimit}`
+  );
+
+  if (confirmation) {
+    addSpaceships(name, pilot, crewLimit);
+  }
+}
+
+function secondMenuOption() {
+  const member = prompt("Qual é o nome do tripulante?");
+  const spaceshipName = prompt(
+    `Para qual nave ${member} deverá ser designado?`
+  );
+
+  const spaceship = findSpaceship(spaceshipName);
+
+  if (spaceship) {
+    const confirmation = confirm(
+      `Confirma a inclusão de ${member} na tripulação da ${spaceship.name}?`
+    );
+
+    if (confirmation) {
+      addCrewMember(member, spaceship);
+    }
+  }
+}
+
+function thirdMenuOption() {
+  const spaceshipName = prompt("Qual é o nome da nave a ser enviada?");
+
+  const spaceship = findSpaceship(spaceshipName);
+
+  if (spaceship) {
+    const confirmation = confirm(
+      `Confirma e envio da ${spaceship.name} na missão?`
+    );
+
+    if (confirmation) {
+      sendInMission(spaceship);
+    }
+  }
+}
+
+function fourthMenuOption() {
+  let list = "Naves Registradas:\n";
+
+  spaceships.forEach(
+    (spaceship: {
+      name: string;
+      pilot: string;
+      crewLimit: number;
+      crew: string[];
+      inMission: boolean;
+    }) => {
+      list += `
+      Nave: ${spaceship.name}
+      Piloto: ${spaceship.pilot}
+      Em missão? ${spaceship.inMission ? "Sim" : "Não"}
+      Tamanho Máximo da Triuplação: ${spaceship.crewLimit}
+      Tripulantes: ${spaceship.crew.length}
+    `;
+
+      spaceship.crew.forEach((member) => {
+        list += `    - ${member}\n`;
+      });
+    }
+  );
+
+  alert(list);
+}
+
+/**
+ * Menu
+ */
+
+let userOption = 0;
+
+while (userOption !== 5) {
+  const menu = `Painel Principal
+     1 - Registrar uma nova nave
+     2 - Adicionar membro da tripulação
+     3 - Enviar nave em missão
+     4 - Listar naves registradas
+     5 - Encerrar
+   `;
+
+  userOption = Number.parseInt(prompt(menu));
+
+  switch (userOption) {
+    case 1:
+      firstMenuOption();
+      break;
+    case 2:
+      secondMenuOption();
+      break;
+    case 3:
+      thirdMenuOption();
+      break;
+    case 4:
+      fourthMenuOption();
+      break;
+    case 5:
+      alert("Encerrando o sistema...");
+      break;
+    default:
+      alert("Opção inválida! Retornando ao painel principal...");
+      break;
   }
 }
