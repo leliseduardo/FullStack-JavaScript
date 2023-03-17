c1eba970285120c90e240bdeb18916b9fc8dbc1f
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2023-03-17 13:01:39 -0300

    Enum - TS

---
+++ 14- TypeScript/2- Primeiro_Codigo/enum_exemp.ts
@@ -0,0 +1,22 @@
+enum Planets {
+  // MERCURIO = 1,
+  MERCURIO,
+  VENUS,
+  TERRA,
+  MARTE,
+}
+
+enum PlanetsString {
+  MERCURIO = "Mercurio",
+  VENUS = "Venus",
+  TERRA = "Terra",
+  MARTE = "Marte",
+}
+
+// Definir regras de permissão
+enum Roles {
+  ADMIN,
+  USER,
+}
+
+Planets.TERRA;
